bd67c2f6d96753d4653d603c0e4230277ab17cba
Jannick Pedersen <jannickpedersen@live.dk>
2023-11-22 14:37:13 +0100

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,307 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "works",
+    title: "Projects",
+  },
+  {
+    id: "diplomas",
+    title: "Diplomas",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+
+const services = [
+  {
+    title: "Frontend / Web Developer",
+    icon: web,
+  },
+  {
+    title: "Backend Developer",
+    icon: mobile,
+  },
+  {
+    title: "Fullstack Developer",
+    icon: backend,
+  },
+  {
+    title: "UI / UX + CSS Frameworks",
+    icon: creator,
+  },
+];
+
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+
+];
+
+
+const experiences = [
+  {
+    title: "Web Integrator PHP",
+    company_name: "Roskilde Techinal School",
+    icon: starbucks,
+    iconBg: "#383E56",
+    date: "Aug 2014 - June 2016",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Computer Science",
+    company_name: "Zealand, Roskilde",
+    icon: tesla,
+    iconBg: "#E6DEDD",
+    date: "Aug 2017 - Jan 2021",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "PBA in Web & App",
+    company_name: "Zealand, Roskilde",
+    icon: shopify,
+    iconBg: "#383E56",
+    date: "Jan 2022 - Aug 2023",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Fullstack Internship",
+    company_name: "NanoScale Simulations",
+    icon: meta,
+    iconBg: "#E6DEDD",
+    date: "Jan 2023 - June 2023",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Fullstack Freelance",
+    company_name: "Orgone Jewels",
+    icon: meta,
+    iconBg: "#E6DEDD",
+    date: "Sep 2023 - November 2023",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+];
+
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+
+const projects = [
+  {
+    name: "NanoScaleSimulations",
+    description:
+      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/",
+    deploy_link: "https://nanoscale.up.railway.app/landing",
+  },
+  {
+    name: "Orgone Jewels",
+    description:
+      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/",
+    deploy_link: "https://next-js-13-e-shop.vercel.app/",
+  },
+  {
+    name: "Serverless Blog",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/",
+    deploy_link: "https://udemynextjsserverlessblog.vercel.app/",
+  },
+];
+
+
+const diplomas = [
+  {
+    title: "Udemy: ReactJS + Firebase",
+    icon: web,
+  },
+  {
+    title: "Udemy: NextJS + MongoDB",
+    icon: mobile,
+  },
+  {
+    title: "Udemy: Wordpress Advance",
+    icon: backend,
+  },
+  {
+    title: "Udemy: MERN Job App",
+    icon: creator,
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects, diplomas };
